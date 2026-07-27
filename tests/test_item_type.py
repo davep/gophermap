1dd555bbd33dee7594242a2bbfb9b0a5a66ab43a
Dave Pearson <davep@davep.org>
2026-07-27 18:55:50 +0100

    :white_check_mark: Add a test for the MIME type of an unknown type

--- tests/test_item_type.py
+++ tests/test_item_type.py
@@ -11,4 +11,10 @@ def test_unknown_type() -> None:
     assert ItemType("x") is ItemType.UNKNOWN
 
 
+##############################################################################
+def test_unknown_mime_type() -> None:
+    """Test that an unknown item type returns ItemType.UNKNOWN."""
+    assert ItemType("x").mime_type == ItemType.UNKNOWN.mime_type
+
+
 ### test_item_type.py ends here
